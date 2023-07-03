884e5f73bac157ea1ec6732d669d554856340feb
Wagner Ferreira <ferreirasi.tk@outlook.com>
2023-07-03 11:14:11 -0300

    ajuste permission

---
+++ app/Models/Password.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(array $array)
+ */
+class Password extends Model
+{
+    use HasFactory;
+
+    protected $table = 'password';
+
+    protected $fillable = [
+        'password',
+        'expiration_date',
+        'status'
+    ];
+
+    protected $casts = [
+        'expiration_date' => 'datetime'
+    ];
+
+    public function scopeAvailable($query)
+    {
+        return $query->where('status', 'available');
+    }
+
+    public function scopeUsed($query)
+    {
+        return $query->where('status', 'used');
+    }
+
+    public function scopeExpired($query)
+    {
+        return $query->where('status', 'expired');
+    }
+
+
+}
